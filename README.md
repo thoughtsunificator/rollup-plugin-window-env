5947e92f45a2ebf53cc9987917f1c2aeb0d0a2d0
Romain Lebesle <oss@thoughtsunificator.me>
2021-12-24 20:31:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rollup-env
+
+rollup plugin to inject given environment variables into the window object..
---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from "fs"
+import { createConfig } from "@thoughtsunificator/config-env"
+
+export default function(config = {}) {
+	const { envPath = ".env.json", configPath = "data/config.json" } = config
+	return {
+		name: 'rollup-env',
+		buildStart(){
+			if (fs.existsSync(envPath)) {
+				this.addWatchFile(envPath)
+			}
+			if (fs.existsSync(configPath)) {
+				this.addWatchFile(configPath)
+			}
+		},
+		banner: () => {
+			const config = createConfig(envPath, configPath)
+			let str = ";(function() {"
+			for(const property in config) {
+				str += `\n\twindow["${property}"] = ${typeof config[property] === "number" ? config[property] : `"${config[property]}"`}\n`
+			}
+			str += "})();"
+			return str
+		}
+	};
+}
